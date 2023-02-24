8713a8d9eb3ea65143a5f9404b039f75d8d6c04d
jean Rondon <jean.rf06@gmail.com>
2023-02-23 23:04:25 -0500

    agregando css y js

--- index.js
+++ index.js
@@ -1,99 +1,108 @@
-let finalizarOperacion = false;
-let defaultValue = false;
-let numberOne = null;
-let numberTwo = null;
 
-let operacionesMatematicas = seleccionarOperacion();
+// let ul = document.getElementsByClassName("cabezera");
+// console.log(ul[2].innerHTML);
 
-if(operacionesMatematicas == 0){
-    alert("Ha cancelado la operacion");
+// let cards =document.getElementsByClassName("congeladosCards");
+
+
+// imprime todo el array
+// for (const el of ul){
+//     console.log(el.innerHTML)
+    // el.innerText = "";        // innerText modifica el html segun la clase de que le de
+// };
+
+
+// let div = document.getElementsByClassName("items");
+
+// div.innerHTML = `<div class="congeladosCards">Pechuga de pollo</div>
+// <div class="congeladosCards">Pierna de pollo</div>
+// <div class="congeladosCards">Carne molida</div>
+// <div class="congeladosCards">Lomo</div>
+// <div class="congeladosCards">carne en trozos</div>
+// <div class="congeladosCards">Costillas de cerdo</div> `;
+
+
+
+// div.className = "fondo-negro"     AGREGA CLASE
+
+
+// const h3=document.createElement("h3");
+// h3.innerText="Soy un elemento h3 y soy el hijo el div che";
+
+// div.appendChild(h3) // AGREGA UN NODO Y CON INNERTEXT AGREGA EL TEXTO
+
+// remove elimina el elemento de escoges
+
+// const primerInput =document.getElementById('primerInput'); //agregar elemento al imput
+// const segundoInput =document.getElementById('segundoInput');
+
+
+// primerInput.value = 'Cesar';
+// segundoInput.value = '17';
+// primerInput.placeholder = 'Ingrese un nombre';
+
+// const array = ["Inicio", "Congelados", 
+// "Galletas", "Tecnologia", "Aguas y Bebidas", "Carrito de compras",
+// "Iniciar sesion"];
+
+// let padre = document.getElementById("app");
+
+// const ol = document.createElement("ol");
+
+// for(const el of array){
+//     const li = document.createElement("li");
+//     li.innerHTML= `<h5>${el}</h5>`;
+//     ol.append(li);
+// };
+
+// padre.append(ol); // apen agrega al principio y APENCHILLD al final
+
+
+// const productos = [
+//     { id : 1, nombre: "carne molida", precio : 25 },
+//     { id : 2, nombre: "carne en trozos ", precio : 24 },
+//     { id : 3, nombre: "pechupa de pollo", precio : 20 },
+//     { id : 4, nombre: "costilla de cerdo", precio : 60 },
+// ];
+
+// for(const producto of productos) {
+//     let contenedor =  document.createElement("div");
+//     contenedor.innerHTML = `<h3> ID: ${producto.id}</h3>
+//                             <p>producto: ${producto.nombre} </p>
+//                             <b>$ ${producto.precio}</b>`;
+
+// };
+// padre.appendChild(productos);
+
+
+
+// const padre = document.querySelectorAll(".div"); // trae todos los q contengan esa clase en array
+// querySelector trae uno solo
+// console.log(padre);
+
+
+
+
+
+// clase 9
+
+
+let boton = document.getElementById("btn1")
+boton.addEventListener("click", respuestaClick)
+function respuestaClick(){
+    console.log("Pechuga de pollo")
 }
-else {
-    while(!finalizarOperacion && !defaultValue ) {
-        if(!operacionesMatematicas){
-            alert("Por favor ingrese un numero");
-        }
-        else {
-            if(operacionesMatematicas < 5 ) {
-                numberOne = parameterOne();
-                numberTwo = parameterTwo();
-            }
-            switch (operacionesMatematicas) {
-                
-                case 1:
-                    finalizarOperacion = true;
-                    resultado = sumar(numberOne,numberTwo);
 
-                    alert("El resultado de la operación es: "+resultado);
-                    break;
-                case 2:
-                    finalizarOperacion = true;
-                    resultado = restar(numberOne,numberTwo);
+// boton.onclick= ()=> {
+//     console.log("Respuesta 2")    // OPCION 2
+// }
 
-                    alert("El resultado de la operación es: "+resultado);
-                    break;
-                case 3:
-                    finalizarOperacion = true;
-                    resultado = multiplicar(numberOne,numberTwo);
-                    
-                    alert("El resultado de la operación es: "+resultado);
-                    break;
-                case 4:
-                    finalizarOperacion = true;
-                    resultado = dividir(numberOne,numberTwo);
 
-                    alert("El resultado de la operación es: "+resultado);
-                    break;
-                case 5:
-                    finalizarOperacion = true;
 
-                    alert("Gracias por utilizar nuestra plataforma, hasta luego!");
-                    break;
-                default:
-                    defaultValue = true;
-                    alert("Ingrese una opcion valida");
-            }
-        }
-    }
-}
 
-function seleccionarOperacion() {
-    let operacionSeleccionada = Number(
-        prompt(
-          "Ingrese que operación desea realizar:\n" +
-            "1 Sumar\n" +
-            "2 Restar\n" +
-            "3 Multiplicar\n" +
-            "4 Dividir\n" +
-            "5 finalizarOperacion\n"
-        )
-      );
-    return operacionSeleccionada;
-}
 
-function parameterOne() {
-    let numberOne = Number(prompt("Ingresa el primer numero"));
-    return numberOne;
-}
 
-function parameterTwo() {
-    let numberTwo = Number(prompt("Ingresa el segundo numero"));
-    return numberTwo;
-}
 
-function sumar(numberOne, numberTwo) {
-    return parseInt(numberOne) + parseInt(numberTwo);
-}
 
-function restar(numberOne, numberTwo) {
-    return parseInt(numberOne) - parseInt(numberTwo);
-}
 
-function multiplicar(numberOne, numberTwo) {
-    return parseInt(numberOne) * parseInt(numberTwo);
-}
-
-function dividir(numberOne, numberTwo) {
-    return parseInt(numberOne) / parseInt(numberTwo);
-}
 
